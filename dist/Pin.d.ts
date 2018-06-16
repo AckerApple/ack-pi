294dc342e412b8a65898e41cb68d905f691438b7
Beautisos <Beautisos@beautisos.local>
2018-06-16 13:56:02 -0400

    x

--- dist/Pin.d.ts
+++
@@ -1,9 +0,0 @@
-import { pin } from "./types";
-import { Pi } from "./pi-sample";
-export declare class Pin implements pin {
-    num: number;
-    Pi: Pi;
-    mode: "high" | "low" | any;
-    type: "INPUT" | "OUTPUT";
-    constructor(num: number, Pi: Pi);
-}
